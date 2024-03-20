6b7e832a996b86c0fd24257d302eff85e651d439
Leandro Lucas <leandrolucasgomesdesousa@gmail.com>
2024-03-20 10:35:07 -0300

    :tada: initial commit

---
+++ Script.js
@@ -0,0 +1,13 @@
+function togglemode() {
+const html = document.documentElement
+if(html.classList.contains ('light')) {
+html.classList. remove ('light')
+} else { html.classList.add ('light')}
+//Pode ser resumido em html.classList.toggle('light')
+
+const img = document.querySelector("#profile img")
+if(html.classList.contains("light")){ img.setAttribute("src","./assets/assets/avatar-light.png")} else {
+img.setAttribute("src","./assets/assets/avatar.png")}
+}
+
+
